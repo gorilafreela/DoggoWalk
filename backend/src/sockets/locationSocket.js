3e733b424eee9d0303ad5df35c35c0f3fb9452d9
gorilafreela <gorilafreela@gmail.com>
2023-05-07 11:09:23 -0300

    ws group chat

--- backend/src/sockets/locationSocket.js
+++ backend/src/sockets/locationSocket.js
@@ -1,4 +1,5 @@
 const WebSocket = require('ws');
+const clients = {};
 
 const locationSocket = (server) => {
   const wss = new WebSocket.Server({ server, path: '/' });
@@ -9,14 +10,26 @@ const locationSocket = (server) => {
     // Send a welcome message to the new client
     socket.send('Welcome to the server!');
 
+    // Generate a unique ID for the client
+    const clientId = Date.now().toString();
+
+    // Store the client by its ID
+    clients[clientId] = socket;
+
     // Listen for messages from the client
     socket.on('message', (message) => {
-      console.log(`Received message: ${message}`);
+      console.log(`${clientId} said: ${message}`);
+      Object.values(clients).forEach((client) => {
+        if (client !== socket) {
+          client.send(`${clientId} said: ${message}`);
+        }
+      });
     });
 
     // Listen for socket close events
     socket.on('close', () => {
       console.log('A client disconnected');
+      delete clients[clientId];
     });
   });
 };
