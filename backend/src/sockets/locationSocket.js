d901cc98dfdd93cb75d352589921ced0fbb26e0a
gorilafreela <gorilafreela@gmail.com>
2023-05-07 11:22:57 -0300

    ws group chat part3

--- backend/src/sockets/locationSocket.js
+++ backend/src/sockets/locationSocket.js
@@ -1,20 +1,36 @@
 const WebSocket = require('ws');
+
 const clients = {};
+const conversations = {};
 
 const locationSocket = (server) => {
   const wss = new WebSocket.Server({ server, path: '/' });
 
-  wss.on('connection', (socket) => {
+  wss.on('connection', (socket, req) => {
     console.log('A new client connected!');
 
-    // Send a welcome message to the new client
-    socket.send('Welcome to the server!');
+    // Parse the query parameters
+    const queryParams = new URLSearchParams(req.url.split('?')[1]);
+    const clientId = queryParams.get('clientId');
 
-    // Generate a unique ID for the client
-    const clientId = Date.now().toString();
+    // Validate the clientId
+    if (!clientId) {
+      socket.send('clientId parameter is missing');
+      socket.close();
+      return;
+    }
+
+    if (clients[clientId]) {
+      socket.send('clientId is already in use');
+      socket.close();
+      return;
+    }
 
     // Store the client by its ID
     clients[clientId] = socket;
+
+    // Send a welcome message to the new client
+    socket.send(`Welcome to the server ${clientId}!`);
 
     // Listen for messages from the client
     socket.on('message', (message) => {
