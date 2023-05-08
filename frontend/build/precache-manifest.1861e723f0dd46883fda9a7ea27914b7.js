fc059ad819b20b5b9c5afc11c05cd9c445f469fc
gorilafreela <gorilafreela@gmail.com>
2023-05-07 21:42:52 -0300

    build

---
+++ frontend/build/precache-manifest.1861e723f0dd46883fda9a7ea27914b7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cada904dc37b2cdc7cbf673818a0601",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d637452ec75e4b785ed",
+    "url": "/static/css/main.a78c4945.chunk.css"
+  },
+  {
+    "revision": "e688565e24ff9a1c97d9",
+    "url": "/static/js/2.46cdf939.chunk.js"
+  },
+  {
+    "revision": "446d4ce71ba781b76349d3153e21572f",
+    "url": "/static/js/2.46cdf939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d637452ec75e4b785ed",
+    "url": "/static/js/main.9c3dc61d.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.9c3dc61d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76de16f27c5532ae7ab",
+    "url": "/static/js/runtime-main.d44c102b.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "ad17ab0a248da58dd0a24a622f473f10",
+    "url": "/static/media/dogwalker.ad17ab0a.jpg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "fe83c0e7c38b175c60bae0427415f294",
+    "url": "/static/media/livetracking.fe83c0e7.png"
+  },
+  {
+    "revision": "e56c58271acf47b03fe486e1993248da",
+    "url": "/static/media/logo-icon.e56c5827.png"
+  },
+  {
+    "revision": "e0469ac0d81771b1da07f57dd2e82d3e",
+    "url": "/static/media/logo.e0469ac0.png"
+  },
+  {
+    "revision": "096949689587ddc1f61b9312fb8c65c8",
+    "url": "/static/media/walk-dog-1.09694968.jpg"
+  }
+]);
